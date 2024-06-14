c5040b7a83c31121738996a10e600d8cb3935a21
BrunoRiver <brunoriveralves@hotmail.com>
2024-06-13 21:29:46 -0300

    primeira versao

---
+++ src/main.js
@@ -0,0 +1,8 @@
+var resultado;
+var tabuada = 2;
+
+for(let numero =0;numero <=10;numero++){
+    resultado = numero * tabuada ;
+    console.log('tabuada do ' + tabuada + ' = 2 * '+ numero ,"= " + resultado)
+}
+
